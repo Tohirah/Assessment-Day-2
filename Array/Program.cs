ef60ca84b821ce319cb6f5927d2c94914001b485
LOLA-BANKS <you@example.com>
2022-08-21 20:20:45 +0100

    first commit

---
+++ Array/Program.cs
@@ -0,0 +1,306 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // QUE 1
+            
+            // Console.WriteLine("enter size of the first Array.");
+            // int arrSize1 = int.Parse(Console.ReadLine());
+            
+            // Console.WriteLine("enter size of the second Array.");
+            // int arrSize2 = int.Parse(Console.ReadLine());
+
+            // int[] arr1 = new int[arrSize1];
+            // int[] arr2 = new int[arrSize2];
+
+            // for(int i = 0; i < arrSize1; i++)
+            // {
+            //     Console.WriteLine("Enter next element");
+            //     arr1[i] = int.Parse(Console.ReadLine());
+            // }
+            // Console.WriteLine("Elements for second Array");
+            // for(int j = 0; j < arrSize2; j++)
+            // {
+            //     Console.WriteLine("Enter next element");
+            //     arr2[j] = int.Parse(Console.ReadLine());
+            // }
+
+            // for(int k = 0; k < arrSize1; k++)
+            // {
+            //     int count = 0;
+            //     for(int m = 0; m < arrSize2; m++)
+            //     {
+            //         if(arr1[k] == arr2[m])
+            //         {
+            //             count++;
+            //         }
+            //     }
+            //     Console.WriteLine(arr1[k] + " occurred " + count + " times");
+            // }
+
+            // QUE 2
+            // Console.WriteLine("enter size of the Array.");
+            // int arrSize = int.Parse(Console.ReadLine());
+
+            // int[] arr = new int[arrSize];
+
+            // for(int i = 0; i < arrSize; i++)
+            // {
+            //     Console.WriteLine("Enter next element");
+            //     arr[i] = int.Parse(Console.ReadLine());
+            // }
+
+            // int small = 0;
+            // for(int j = 0; j < arr.Length; j++)
+            //     {
+            //         for(int i = j+1; i < arr.Length; i++)
+            //         {
+            //             if(arr[i] < arr[j])
+            //             {
+            //                 small = arr[i];
+            //                 arr[i] = arr[j];
+            //                 arr[j] = small; 
+            //             }
+            //         }
+            //     }
+            //     Console.WriteLine("Check");
+            // int smallest = arr[0];
+            // int largest = arr[arrSize - 1];
+            // for(int w = smallest + 1; w < largest; w++)
+            // {
+            //     Console.WriteLine(w);
+            // }
+
+            //     QUE 3
+            // Console.WriteLine("Enter size of the Array.");
+            // int arrSize = int.Parse(Console.ReadLine());
+
+            // int[] arr = new int[arrSize];
+
+            // for(int i = 0; i < arrSize; i++)
+            // {
+            //     Console.WriteLine("Enter next element");
+            //     arr[i] = int.Parse(Console.ReadLine());
+            // }
+            // for(int j = 0; j < arrSize; j++)
+            // {
+            //     for(int k = j + 1; k < arrSize; k++)
+            //     {
+            //         if(arr[j] == arr[k])
+            //         {
+            //             arr[k] = 0;
+            //         }
+            //     }
+            // }
+
+            // Console.Write("New elements are: ");
+            // for(int i = 0; i < arrSize; i++)
+            // {
+            //     if(arr[i] == 0)
+            //     continue;
+            //     Console.Write(arr[i] + ", ");
+            // }
+            // Console.WriteLine("");
+
+            // QUE 4
+            // Console.WriteLine("Enter the number of subjects");
+            // int length = int.Parse(Console.ReadLine());
+
+            // int[] score = new int[length];
+            // string[] subject = new string[length]; 
+
+            // for(int j = 0; j < length; j++)
+            // {
+            //     Console.WriteLine("Enter the name of the next subject");
+            //     subject[j] = Console.ReadLine();
+
+            //     Console.WriteLine("Enter the score for the subject");
+            //     score[j] = int.Parse(Console.ReadLine());
+            // }
+
+            // Console.Write("Your new score is: ");
+            // for(int k = 0; k < length; k++)
+            // {
+            //     if(score[k] < 50)
+            //     {
+            //         score[k] +=10;
+            //     }
+            //     else if(score[k] < 60 && score[k] >= 50)
+            //     {
+            //         score[k] +=2;
+            //     }
+            //     else if(score[k] > 70)
+            //     {
+            //         score[k] -=5;
+            //     }
+            //     else
+            //     {
+            //         score[k] +=4;
+            //     }
+
+            // if(k < length - 1)
+            //     {
+            //         Console.Write(subject[k] + "-" + score[k] + ", ");
+            //     }
+            //     else
+            //     {
+            //         Console.Write(subject[k] + "-" + score[k]);
+            //     }
+
+            // }
+            // Console.WriteLine("");
+
+            // QUE 5
+            // Console.WriteLine("Enter size of the Array.");
+            // int length = int.Parse(Console.ReadLine());
+
+            // int[] arrDuplicate = new int[length];
+
+            // int count = 1;
+            // int largestCount = 0;
+            // int largestNum = 0;
+
+            // for(int i = 0; i < arrDuplicate.Length; i++)
+            // {
+            //     Console.WriteLine("Enter next Element");
+            //     arrDuplicate[i] = int.Parse(Console.ReadLine());
+            // }
+
+            // for(int j = 0; j < arrDuplicate.Length - 1; j++)
+            // {
+            //     for(int k = j + 1; k <arrDuplicate.Length; k++)
+            //     {
+            //         if(arrDuplicate[j] == arrDuplicate[k])
+            //         {
+            //             count++;
+            //             largestNum = arrDuplicate[j];
+            //         }
+            //         else
+            //         {
+            //         break;
+            //         }
+            //     }
+            //     if(count > largestCount)
+            //     {
+            //         largestCount = count;
+            //         largestNum = arrDuplicate[j];
+            //     }
+            //     count = 1;
+            // }
+
+            // if(largestCount > 0)
+            // {
+            //     for(int k = 1; k <= largestCount; k++)
+            //     {
+            //         if(k < largestCount)
+            //         {
+            //             Console.Write(largestNum + ", ");
+            //         }
+            //         else
+            //         {
+            //             Console.Write(largestNum);
+            //         }
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Equal number of consecutive equal sequence for all elements");
+            // }
+            
+
+            // QUE 7 - WIP
+            Console.WriteLine("Enter size of the Array.");
+            int len = int.Parse(Console.ReadLine());
+
+            int[] arrMerge1 = new int[len];
+            int[] arrMerge2 = new int[len];
+            int[] mergedArr = new int[len * 2];
+
+            for(int i = 0; i < len; i++)
+            {
+                Console.WriteLine("Enter next element for first array");
+                arrMerge1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("SECOND ARRAY ELEMENTS");
+            for(int i = 0; i < len; i++)
+            {
+                Console.WriteLine("Enter next element for second array");
+                arrMerge2[i] = int.Parse(Console.ReadLine());
+            }
+
+            int n = 0;
+
+            for(int k = 0; k < mergedArr.Length; k++)
+            {
+                if(k < len)
+                {
+                    mergedArr[k] = arrMerge1[k];
+                }
+                else
+                {
+                    mergedArr[k] = arrMerge2[n];
+                    n++;
+                }
+            }
+
+            int biggest = 0;
+            
+            for(int i = 0; i < mergedArr.Length; i++)
+            {
+                    for(int j = i + 1; j < mergedArr.Length; j++)
+                    {
+                        if(mergedArr[j] < mergedArr[i])
+                        {
+                            biggest = mergedArr[j];
+                            mergedArr[j] = mergedArr[i];
+                            mergedArr[i] = biggest;
+                        }
+                    }
+                }
+
+            foreach (int item in mergedArr)
+            {
+                Console.WriteLine(item);
+            }
+
+            // QUE 8
+            // Console.WriteLine("Enter size of the Array.");
+            // int len = int.Parse(Console.ReadLine());
+
+            // int[] arr = new int[len];
+
+            // for(int i = 0; i < len; i++)
+            // {
+            //     Console.WriteLine("Enter next element");
+            //     arr[i] = int.Parse(Console.ReadLine());
+            // }
+
+            // int biggest = 0;
+            // int secondbiggest = 0;
+
+            // for(int k = 0; k < len; k++)
+            // {
+            //     if(arr[k] > biggest)
+            //     {
+            //         secondbiggest = biggest;
+            //         biggest = arr[k];
+            //     }
+            //     else if(secondbiggest < arr[k] && arr[k] < biggest)
+            //     {
+            //         secondbiggest = arr[k];
+            //     }
+            // }
+
+            // Console.WriteLine(secondbiggest);
+
+
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // QUE 1
+            // Console.WriteLine("Enter the word");
+            // string word = Console.ReadLine();
+
+            // Console.WriteLine(MidChar(word));
+
+            // QUE 2
+            // Console.WriteLine("Enter the word");
+            // string word = Console.ReadLine();
+
+            // Console.WriteLine(CountVowel(word));
+
+            //  QUE 6
+            Console.WriteLine("Enter the number");
+            int even = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CheckEven(even));
+
+
+            // QUE 7
+            // Console.WriteLine("Enter the number");
+            // string num = Console.ReadLine();
+
+            // ReverseNum(num);
+
+            // QUE 8
+            // Console.WriteLine("Enter a non negative number - i.e. number greater than one.");
+            // int pali = int.Parse(Console.ReadLine());
+
+            // if(pali < 1)
+            // {
+            //     Console.WriteLine("Invalid Input.");
+            // }
+            // else
+            // {
+            //     Palindrome(pali);
+            // }
+
+            // QUE 9
+            // Console.WriteLine("Enter the number");
+            // string word = Console.ReadLine();
+
+            // Console.WriteLine(SumNum(word));
+
+            //QUE 10
+            // Console.WriteLine("Enter the number");
+            // int num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(LastNum(num));
+        }
+
+        // QUE 1
+        public static char MidChar(string k)
+        {
+            if(k.Length % 2 == 0)
+            {
+                return(k[((k.Length - 1) / 2) + 1]);
+            }
+            else
+            {
+                return(k[k.Length / 2]);
+            }
+            
+        }
+
+        // QUE 2
+        public static int CountVowel(string k)
+        {
+            int count = 0;
+            for(int m = 0; m < k.Length; m++)
+            {
+                if(k[m] == 'a' || k[m] == 'e' || k[m] == 'i' || k[m] == 'o' || k[m] == 'u')
+                {
+                    count++;
+                }
+            }
+
+            return(count);
+        }
+
+        // QUE 4 - WIP
+        public static bool Password(string k)
+        {
+            bool validPass = true;
+            for(int m = 0; m < k.Length; m++)
+            {
+                if(k.Length > 9)
+                {
+                }
+                else
+                {
+                    validPass = false;
+                }
+            }
+
+            return(validPass);
+        }
+
+        // QUE 6
+        public static bool CheckEven(int k)
+        {
+            bool isEven = true;
+            for(;k > 1;k /=10)
+            {
+                int j = k % 10;
+                if(j % 2 == 0)
+                {continue;}
+                else
+                {
+                    isEven = false;
+                    break;
+                }
+            }
+
+            return(isEven);
+        }
+
+        // QUE 7
+        public static void ReverseNum(string k)
+        {
+            for(int i = k.Length - 1; i >= 0; i--)
+            {
+                Console.Write(k[i]);
+            }
+        }
+
+        // QUE 8
+        public static void Palindrome(int k)
+        {
+            string m = k.ToString();
+            string j = "";
+            for(int i = m.Length - 1; i >= 0; i--)
+            {
+                j +=m[i];
+            }
+
+            Console.WriteLine( j == m);
+
+        }
+
+        //QUE 9
+        public static int SumNum(string k)
+        {
+            int sum = 0;
+            for(int i = 0; i < k.Length; i++)
+            {
+                sum +=int.Parse(k[i].ToString());
+            }
+
+            return(sum);
+        }
+
+        // QUE 10
+        public static string LastNum(int k)
+        {
+            int lastNum =k % 10;
+            string lastNumToWord = "";
+            switch (lastNum)
+            {
+                case 0:
+                lastNumToWord = "Zero";
+                break;
+                case 1:
+                lastNumToWord = "One";
+                break;              
+                case 2:
+                lastNumToWord = "Two";
+                break; 
+                case 3:
+                lastNumToWord = "Three";
+                break; 
+                case 4:
+                lastNumToWord = "Four";
+                break;
+                case 5:
+                lastNumToWord = "Five";
+                break;
+                case 6:
+                lastNumToWord = "Six";
+                break;
+                case 7:
+                lastNumToWord = "Seven";
+                break;
+                case 8:
+                lastNumToWord = "Eight";
+                break;
+                case 9:
+                lastNumToWord = "Nine";
+                break;
+                default:
+                break;
+            }
+
+            return(lastNumToWord);
+        }
+    }
+}
